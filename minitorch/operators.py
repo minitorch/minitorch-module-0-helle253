d12edc907dc96481ba720c92566f02299b8cd46f
helle253 <nathanhellbhoy@gmail.com>
2024-08-30 13:34:24 -0500

    whitespace and add another ternary

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -37,63 +37,60 @@ from typing import Callable, Iterable
 
 
 def mul(x: float, y: float) -> float:
-  return x * y
+    return x * y
 
 def id(input: any) -> any:
-  return input
+    return input
 
 def add(x: float, y: float) -> float:
     return x + y
 
 def neg(x: float) -> float:
-  return -1 * x
+    return -1 * x
 
 def lt(x: float, y: float) -> float:
-  return 1.0 if x < y else 0.0
+    return 1.0 if x < y else 0.0
 
 def eq(x: float, y: float) -> float:
-  return 1.0 if x == y else 0.0
+    return 1.0 if x == y else 0.0
 
 def max(x: float, y: float) -> float:
-  return x if x > y else y
+    return x if x > y else y
 
 # $f(x) = |x - y| < 1e-2$q
 def is_close(x: float, y: float) -> float:
-  return abs(x - y) < 1e-2
+    return abs(x - y) < 1e-2
 
 # $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
 def sigmoid(x: float) -> float:
-  if x >= 0:
-    return 1.0 / (1.0 + math.e ** -x)
-  else:
-    return (math.e ** x) / (1.0 + math.e ** x)
+    if x >= 0:
+        return 1.0 / (1.0 + math.e ** -x)
+    else:
+        return (math.e ** x) / (1.0 + math.e ** x)
 
 def relu(x: float) -> float:
-  if x > 0:
-    return x
-  else:
-    return 0
+    if x > 0:
+        return x
+    else:
+      return 0
 
 def log(x: float) -> float:
-  return math.log(x, math.e)
+    return math.log(x, math.e)
 
 def exp(x: float) -> float:
     return math.e * x
 
 def inv(x: float) -> float:
-  return 1.0 / x
+    return 1.0 / x
 
 def log_back(x: float, y: float) -> float:
-  return y / x
+    return y / x
 
 def inv_back(x: float, y: float) -> float:
-  return y / x ** 2
+    return y / x ** 2
 
 def relu_back(x: float, y: float) -> float:
-  if x > 0:
-    return y
-  else:
-    return 0
+    return y if x > 0 else 0
 
 # ## Task 0.3
 
@@ -111,13 +108,13 @@ def relu_back(x: float, y: float) -> float:
 # - prod: take the product of lists
 
 def negList(l: Iterable[float]) -> Iterable[float]:
-  return map(lambda x: neg(x), l)
+    return map(lambda x: neg(x), l)
 
 def addLists(l: Iterable[float], r: Iterable[float]) -> Iterable[float]:
-  return map(lambda x, y: add(x, y), l, r)
+    return map(lambda x, y: add(x, y), l, r)
 
 def sum(l: Iterable[float]) -> float:
-  return reduce(lambda x, y: add(x, y), l, 0)
+    return reduce(lambda x, y: add(x, y), l, 0)
 
 def prod(l: Iterable[float]) -> float:
-  return reduce(lambda x, y: mul(x, y), l, 1)
+    return reduce(lambda x, y: mul(x, y), l, 1)
